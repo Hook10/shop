b62e6b191f8a4b211a312209abfbdc8909085a6c
AskarSap <sapasask@gmail.com>
2023-08-05 19:21:27 +0600

    Initial commit

---
+++ shopwebparent/shopfrontend/src/main/java/com/hook/ShopFrontEnd.java
@@ -0,0 +1,11 @@
+package com.hook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopFrontEnd {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopFrontEnd.class, args);
+    }
+}
